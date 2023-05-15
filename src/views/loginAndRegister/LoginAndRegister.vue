5ec12ad6824cda332c73ee7bd8600a16d8231bef
HASAXIN <osh1512652759@outlook.com>
2023-05-15 16:59:01 +0800

    完善登录注册界面

--- src/views/loginAndRegister/LoginAndRegister.vue
+++ src/views/loginAndRegister/LoginAndRegister.vue
@@ -1,11 +1,20 @@
 <template>
   <div class=''>
-    333
+    <el-tabs class="demo-tabs" v-model="activeName" @tab-click="handleClick">
+    <el-tab-pane label="登录" name="login">登录</el-tab-pane>
+    <el-tab-pane label="注册" name="register">注册</el-tab-pane>
+  </el-tabs>
   </div>
 </template>
 
 <script lang='ts' setup>
-// import { ref } from 'vue';
+import { ref } from 'vue'
+import type { TabsPaneContext } from 'element-plus'
 
+const activeName = ref('login')
+
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+  console.log(tab, event)
+}
 </script>
 <style lang='scss' scoped></style>
